d78e30666daa43f349d12ddef8c26a0bf30e8a5c
B-Mugnol <bruno_mugnol@hotmail.com>
2022-08-01 22:14:12 +0200

    added 2 new functions to utilities

--- src/utilities/count_lines.c
+++ src/utilities/count_lines.c
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utilities.h                                        :+:      :+:    :+:   */
+/*   count_lines.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/01 04:37:48 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/08/01 22:13:41 by bmugnol-         ###   ########.fr       */
+/*   Created: 2022/08/01 21:49:14 by bmugnol-          #+#    #+#             */
+/*   Updated: 2022/08/01 21:49:22 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILITIES_H
-# define UTILITIES_H
+#include "utilities.h"
 
-#include <stdlib.h>
-#include <string.h>
+size_t	count_lines(char **matrix)
+{
+	size_t	i;
 
-size_t	count_lines(char **matrix);
-void	ft_free_char_matrix(char ***m);
-char	**ft_split(char const *s, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-char	*ft_utoa(unsigned int n);
-
-#endif
+	i = 0;
+	while (matrix && matrix[i])
+		i++;
+	return (i);
+}
