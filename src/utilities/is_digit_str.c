3cc7cfe64b0e6244da8f058ea2958a38744425f9
B-Mugnol <bruno_mugnol@hotmail.com>
2022-08-02 03:57:19 +0200

    added is_digit_str function

--- src/utilities/is_digit_str.c
+++ src/utilities/is_digit_str.c
@@ -1,26 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utilities.h                                        :+:      :+:    :+:   */
+/*   is_digit_str.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/01 04:37:48 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/08/02 01:16:45 by bmugnol-         ###   ########.fr       */
+/*   Created: 2022/08/02 01:11:07 by bmugnol-          #+#    #+#             */
+/*   Updated: 2022/08/02 01:14:57 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef UTILITIES_H
-# define UTILITIES_H
+#include <ctype.h>
+#include "utilities.h"
 
-#include <stdlib.h>
-#include <string.h>
-
-size_t	count_lines(char **matrix);
-int		is_digit_str(const char *str);
-void	ft_free_char_matrix(char ***m);
-char	**ft_split(char const *s, char c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-size_t	ft_strlcpy(char *dest, const char *src, size_t size);
-
-#endif
+int	is_digit_str(const char *str)
+{
+	if (!str || !*str || !isdigit(*str))
+		return (0);
+	str++;
+	while (*str)
+	{
+		if (!isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
