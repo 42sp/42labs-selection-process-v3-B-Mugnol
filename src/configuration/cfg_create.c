42d8c569418dcf6d4f5cb69d9de0320cf17e4bca
B-Mugnol <bruno_mugnol@hotmail.com>
2022-08-02 04:04:18 +0200

    added create protocol-specific attribute

---
+++ src/configuration/cfg_create.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cfg_create.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 04:20:44 by bmugnol-          #+#    #+#             */
+/*   Updated: 2022/08/02 01:25:32 by bmugnol-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "monitoring.h"
+#include "define.h"
+#include "cfg.h"
+#include "utilities.h"
+#include <string.h>
+#include <stdlib.h>
+
+void	*create_http(char **config)
+{
+	t_http	*attr;
+
+	if (count_lines(config) != 6)
+	{
+		if (count_lines(config) > 6)
+			fprintf(stderr, "col:	6");
+		else if (count_lines(config) < 6)
+			fprintf(stderr, "col:	%lu", count_lines(config));
+		return (NULL);
+	}
+	attr = malloc(sizeof(t_http));
+	attr->base_attributes.name = strdup(config[0]);
+	attr->base_attributes.address = strdup(config[2]);
+	attr->method = get_http_method(config[3]);
+	attr->expected_status_code = atoi(config[4]);
+	attr->base_attributes.range = strtoul(config[5], NULL, 10);
+	if (
+		attr->expected_status_code < 100 || attr->expected_status_code >= 600
+		|| !is_digit_str(config[4]) || !is_digit_str(config[5]))
+	{
+		delete_http_config(attr);
+		attr = NULL;
+	}
+	return (attr);
+}
+
+void	*create_ping(char **config)
+{
+	t_ping	*attr;
+
+	if (count_lines(config) != 4)
+	{
+		if (count_lines(config) > 4)
+			fprintf(stderr, "col:	4");
+		else if (count_lines(config) < 4)
+			fprintf(stderr, "col:	%lu", count_lines(config));
+		return (NULL);
+	}
+	attr = malloc(sizeof(t_ping));
+	attr->base_attributes.name = strdup(config[0]);
+	attr->base_attributes.address = strdup(config[2]);
+	attr->base_attributes.range = strtoul(config[3], NULL, 10);
+	if (!is_digit_str(config[3]))
+	{
+		delete_ping_config(attr);
+		attr = NULL;
+	}
+	return (attr);
+}
+
+void	*create_dns(char **config)
+{
+	t_dns	*attr;
+
+	if (count_lines(config) != 5)
+	{
+		if (count_lines(config) > 5)
+			fprintf(stderr, "col:	5");
+		else if (count_lines(config) < 5)
+			fprintf(stderr, "col:	%lu", count_lines(config));
+		return (NULL);
+	}
+	attr = malloc(sizeof(t_dns));
+	attr->base_attributes.name = strdup(config[0]);
+	attr->base_attributes.address = strdup(config[2]);
+	attr->base_attributes.range = strtoul(config[3], NULL, 10);
+	attr->dns_server = strdup(config[4]);
+	if (!is_digit_str(config[3]))
+	{
+		delete_dns_config(attr);
+		attr = NULL;
+	}
+	return (attr);
+}
