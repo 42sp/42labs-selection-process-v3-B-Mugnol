9cbf4504f0cd89f1e52c4232961d4b53cf1a9d4e
B-Mugnol <bruno_mugnol@hotmail.com>
2022-08-02 23:25:20 +0200

    added service lst and its respective clearing

--- src/main.c
+++ src/main.c
@@ -6,17 +6,19 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 20:00:09 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/07/31 23:57:21 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/08/02 23:24:17 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "monitoring.h"
 #include "define.h"
+#include "cfg.h"
 
 int	main(int argc, char **argv)
 {
 	FILE	*config_file;
 	FILE	*log_file;
+	t_list	*service_lst;
 
 	if (program_param_verifier(argc, argv) == -1)
 		return (EXIT_FAILURE);
@@ -28,5 +30,9 @@ int	main(int argc, char **argv)
 		close_fstream(log_file);
 		return (EXIT_FAILURE);
 	}
+	service_lst = get_service_list(config_file);
+	close_fstream(config_file);
+	close_fstream(log_file);
+	ft_lstclear(&service_lst, delete_service);
 	return (EXIT_SUCCESS);
 }
