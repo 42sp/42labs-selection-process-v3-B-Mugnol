d3a45ca9403b42e283bc75346a1fcfb9dbcfde67
B-Mugnol <bruno_mugnol@hotmail.com>
2022-08-03 18:12:53 +0200

    beautify

--- src/configuration/cfg_get.c
+++ src/configuration/cfg_get.c
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 01:25:17 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/08/03 18:09:51 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/08/03 18:12:41 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #include "utilities.h"
 #include "define.h"
 
-static t_protocol		get_protocol(char *protocol);
-static void				get_attributes(char **config, t_service *svc);
+static t_protocol	get_protocol(char *protocol);
+static void			get_attributes(char **config, t_service *svc);
 
 t_service	*get_service(char *line, size_t line_number)
 {
