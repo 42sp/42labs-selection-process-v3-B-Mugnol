4f1c98682c86f3fe578dcd92dee22048c6342268
B-Mugnol <bruno_mugnol@hotmail.com>
2022-08-03 16:39:18 +0200

    beautify

--- inc/define.h
+++ inc/define.h
@@ -6,7 +6,7 @@
 /*   By: bmugnol- <bmugnol-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/28 19:52:12 by bmugnol-          #+#    #+#             */
-/*   Updated: 2022/08/03 00:10:58 by bmugnol-         ###   ########.fr       */
+/*   Updated: 2022/08/03 16:39:05 by bmugnol-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,14 @@
 // Allowed flag
 # define FLAG	"--simplify"
 
+// FILES
 // Configuration file
 # define I_FILE	"monitoring.db"
 // Output file
 # define O_FILE	"monitoring.log"
-
-// Configurations' separators in configuration file
+// Separators in configuration file
 # define COLUMN_SEPARATOR	"\t"
 # define LINE_SEPARATOR		"\n"
-
 /* If one of these is the first character in a line, the subsequent characters will be ignored until the line ends */
 # define IGN_LINE_START		"#\n"
 
